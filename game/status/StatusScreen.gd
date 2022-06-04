697447e912fd1beeb67098ed8d1fb1df1430410a
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-06-03 23:27:22 -0400

    Updated status screen and controllers image.

--- game/status/StatusScreen.gd
+++ game/status/StatusScreen.gd
@@ -8,15 +8,13 @@ func _ready():
 	if GameState.current_score > 0:
 		$LastScoreValue.text = "%d" % [ GameState.current_score ]
 	else:
-		$LastScoreLabel.visible = false
-		$LastScoreValue.visible = false
+		$LastScoreValue.text = "-"
 
 	# Show best score
 	if GameState.best_score > 0:
 		$BestScoreValue.text = "%d" % [ GameState.best_score ]
 	else:
-		$BestScoreLabel.visible = false
-		$BestScoreValue.visible = false
+		$BestScoreValue.text = "-"
 
 
 func _on_StartButton_pressed():
