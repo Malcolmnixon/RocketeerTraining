c4013af51781f49057cea41c131660e399107266
Malcolm Nixon <Malcolm.nixon@gmail.com>
2023-01-20 20:59:38 -0500

    Upgraded to latest godot-xr-tools. Added support for WebXR. Added ghost-hands for climbing. Modified game lighting.

--- upload-to-itch.bat
+++ upload-to-itch.bat
@@ -1,9 +1,11 @@
 SET CURRENT_DIR=%~dp0
-SET APK=%CURRENT_DIR%/build/RocketeerTraining.apk
+SET APK=%CURRENT_DIR%/build/quest/RocketeerTraining.apk
 SET WINDOWS=%CURRENT_DIR%/build/windows/
+SET WEBXR=%CURRENT_DIR%/build/webxr/
 
 echo Uploading to Itch...
 butler -v push "%APK%" "malcolmnixon/rocketeer-training:android"
 butler -v push "%WINDOWS%" "malcolmnixon/rocketeer-training:windows"
+butler -v push "%WEBXR%" "malcolmnixon/rocketeer-training:html5"
 
 echo Done.
--- upload-to-itch.sh
+++ upload-to-itch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 CURRENT_DIR=$(dirname "$(readlink -f "$0")")
-APK="$CURRENT_DIR/build/RocketeerTraining.apk"
+APK="$CURRENT_DIR/build/quest/RocketeerTraining.apk"
 WINDOWS="$CURRENT_DIR/build/windows/"
+WEBXR="$CURRENT_DIR/build/webxr/"
 
 echo Uploading to Itch...
 butler -v push "$APK" "malcolmnixon/rocketeer-training:android"
 butler -v push "$WINDOWS" "malcolmnixon/rocketeer-training:windows"
+butler -v push "$WEBXR" "malcolmnixon/rocketeer-training:webxr"
 
 echo Done.
